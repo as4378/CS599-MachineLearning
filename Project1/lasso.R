93ca79eaa6d58610bfb7f906c9217eb2d83d835c
Anuraag Srivastava <44287697+as4378@users.noreply.github.com>
2019-10-04 17:11:53 -0700

    Update lasso.R

--- Project1/lasso.R
+++ Project1/lasso.R
@@ -1,1 +1,26 @@
 
+lasso <- function(Y, X){
+ # check for class of Y and X
+  if(class(X) != "data.frame"){
+    X <- as.data.frame(X)
+  }
+  if(class(Y) != "data.frame"){
+    Y <- as.data.frame(Y)
+  }
+  
+  #standardize the predictors to have mean 0 and unit norm
+  for(i in 1:ncol(X)){
+    X[,i] <- (X[,i] - mean(X[,i]))/sd(X[,i])
+  }
+  
+  #initial residual
+  r = Y[,] - mean(Y[,])
+  
+  # initialize the initial coefficient matrix as all 0s
+  beta <- matrix(0, nrow = ncol(X), ncol = 1)
+  
+  # find the correlation of residuals with active set of variables
+  c <- cor(X, r)
+  
+  #TODO: next steps
+}
